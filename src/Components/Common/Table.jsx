98706aea500f2c9676ac7612e39ab42b0be383bc
nagaharshitha20 <knagaharshitha2005@gmail.com>
2025-07-24 14:44:14 +0530

    Working on Table Layout

--- src/Components/Common/Table.jsx
+++ src/Components/Common/Table.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Table = () => {
   return (
     <div>
-      euhd
+     hi
     </div>
   )
 }
--- src/Components/Orders/OrderList/OrderListLayout.jsx
+++ src/Components/Orders/OrderList/OrderListLayout.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Table from '../../Common/Table'
 
 const OrderListLayout = () => {
   return (
-    <div>OrderListLayout</div>
+    <div><Table/>
+    </div>
   )
 }
 
