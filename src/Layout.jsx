9f008a74e183a9c02c82f5e3976bdb1121273e20
Joshika-pachi <pmsjacc@gmail.com>
2025-07-24 15:29:33 +0530

    navbar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Footer from './Components/Footer/Footer';
 
 const Layout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(prev => !prev);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+      <Navbar toggleSidebar={toggleSidebar} projectName={"Icy Tales"} logoImage={"#"}/>
+
       <div className="flex flex-1">
-        <Sidebar />
-        <main className="flex-grow p-6 mt-16"> {/* mt-16 leaves space if Navbar is fixed */}
-          {/* You can remove mt-16 if Navbar is not fixed */}
-          <p className='text-red-300'>hiii</p>
-          <Outlet />
+        <Sidebar isSidebarOpen={isSidebarOpen} />
+
+        <main className="flex-grow flex flex-col mt-16 p-2">
+          <div className="flex-grow">
+            <p className="text-red-300">hiii</p>
+            <Outlet />
+          </div>
+
+          <Footer projectName={"HeavenLyte"} rightsOwner={"Joshika"}/>
         </main>
       </div>
-      <Footer />
     </div>
   );
 };
