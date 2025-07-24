8b94a3ce99b7d8d5ea61b2ca1b95b22e326006ad
sandeep10704 <saivenkatasandeep3@gmail.com>
2025-07-24 11:38:36 +0530

    correct file stuture and code, correct libs

---
+++ src/Router.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Layout';
+import Dashboard from './Pages/Dashboard';
+import Orders from './Pages/Orders';
+import Products from './Pages/Products';
+import Categories from './Pages/Categories';
+import Customers from './Pages/Customers';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '', element: <Dashboard /> },
+      { path: 'orders', element: <Orders /> },
+      { path: 'products', element: <Products /> },
+      { path: 'categories', element: <Categories /> },
+      { path: 'customers', element: <Customers /> },
+    ],
+  },
+]);
+
+export default router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,10 @@
-// main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './Layout';
-import Dashboard from './Pages/Dashboard';
-import Orders from './Pages/Orders';
-import Products from './Pages/Products';
-import Categories from './Pages/Categories';
-import Customers from './Pages/Customers';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { path: '', element: <Dashboard /> },
-      { path: 'orders', element: <Orders /> },
-      { path: 'products', element: <Products /> },
-      { path: 'categories', element: <Categories /> },
-      { path: 'customers', element: <Customers /> },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
